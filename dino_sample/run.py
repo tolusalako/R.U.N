0ef8ff319b93c10373ab079ce8d86922b30e4051
Tolu <tsalako@uci.edu>
2015-12-28 22:49:38 -0800

    Added dino sample.

--- dino_sample/run.py
+++ dino_sample/run.py
@@ -1,24 +1,19 @@
-from gameobject import Object
+#Toluwanimi Salako  (www.salakotech.com) 
 import gamewindow
 from os import listdir
 import Tkinter as TK
 from PIL import ImageTk
 from datetime import datetime
-import sys, Queue
+import sys
 from collections import defaultdict
 import time
 
 controls = {
-	'top' : 0x58,
-	'right': 0x27,
-	'left': 0x25
+	'jump' : 0x20,
 }
 
-
-
 class Tile(tuple):
-	def __init__(self, location, top = None, left = None, right = None, topright = None, topleft = None,
-		score = 0, cost = 0):
+	def __init__(self, location):
 		tuple.__init__(location)
 		self.x = self.__getitem__(0)
 		self.y = self.__getitem__(1)
@@ -35,7 +30,7 @@ class RunAI():
 		self.destroy = False
 		self.main_char= main_character
 		self.char_location = None
-		self.tile_size = (33, 33)
+		self.tile_size = (38, 38)
 		self.current_tree = defaultdict(int) #key = location, value = Tile
 
 		self.window = gamewindow.GameWindow(name, self.template_path)
@@ -55,6 +50,7 @@ class RunAI():
 		self.master = TK.Tk()
 		self.base = TK.Frame(self.master)
 		self.master.wm_title("RunAI")
+		#self.master.iconbitmap('icon.ico')
 		self.canvas = TK.Canvas(master = self.base, background = "#FFFFFF", height = self.height*self.downsize,
 		 width = self.width*self.downsize)
 		self.canvas.grid(row = 0, column = 0, sticky = TK.W)
@@ -63,14 +59,16 @@ class RunAI():
 		self.save_btn.grid(row = 1, columnspan = 20, sticky = TK.W + TK.E)
 
 		self.base.pack()
+		# self.preview_loop()
 		self.update_status()
-		# self.preview_loop() #If you want preview, comment update_status(), run(), and master.withdraw()
 		self.run()
 		self.master.withdraw()
 		self.master.mainloop()
 
 	def update_status(self):
 		self.temp_img = self.window.capture()
+		width, height = self.temp_img.size
+		self.temp_img = self.temp_img.crop((160, 90, width - 170, height - 610))
 		if not self.temp_img is None:
 			self.found_objects = gamewindow.find_objects_as_objects(self.temp_img, 
 				[self.template_path + o for o in self.objects], self.threshold)
@@ -87,31 +85,15 @@ class RunAI():
 		x, y = root
 		w, h = self.tile_size
 		heighest = 0
-		pit_length = 0
 		for i in xrange(w,steps*w, w):
 			front = self.get_tile((x+i, y))
 			if front is None: #Out of bounds or No obstacle.
 				pass
 			else:
 				s = self.status[front]
-				if s == 'goomba':
+				if s == 'obstacle':
 					heighest = max(heighest, 1)
-				elif s == 'block':
-					height = 1
-					for j in xrange(h,steps*h, h):
-						above = self.get_tile((x+i, y - j))
-						if above is None:
-							break
-						elif self.status[above] == 'block':
-							height += 1
-					heighest = max(heighest, height)
-				elif s == 'pipe':
-					heighest = 4
-				else: #If front is clear, check below it for pit
-					below = self.get_tile((x+i, y+w))
-					if below is not None and self.status[below] == '': #PIT!
-						pit_length += 1
-		return heighest, pit_length
+		return heighest
 
 	def get_tile_in_direction(self, start, direction):
 		x,y = start
@@ -135,6 +117,13 @@ class RunAI():
 			pass
 		return result
 
+	def print_status(self):
+		status = self.status
+		for s in status.keys():
+			print str(s) + ': ' + status[s]
+		sys.stdout.flush()
+		self.master.after(3000, self.print_status)
+
 	def generate_tiles(self):
 		self.tiles = []
 		width, height = self.window.size()
@@ -148,9 +137,6 @@ class RunAI():
 	def save_temp(self):
 		self.preview_img.save(str(datetime.now().strftime("%I-%M %p")) + '.png')
 
-	def __on_exit_preview(self):
-		self.destroy = True
-
 	def preview_loop(self):
 		self.preview_img = self.window.capture()
 		if not self.preview_img is None:
@@ -162,29 +148,23 @@ class RunAI():
 			self.master.after(1, self.preview_loop)
 
 	def run(self):
-		self.current_tree.clear() #clear
 		status = self.status.items()
 		root = None
 		for k,v in status:
-			if v == 'mario':
+			if v == 'dino':
 				root = k
 				break
 
-
-		KEY_PRESS = 0.3
+		KEY_PRESS = 0.2
 		KEY_PRESS_SMALL = 0.15
 		if root is not None:
-			obstacle, pit = self.check_forward_for_obstacles(root, steps = 5)
-			print obstacle, pit
+			obstacle = self.check_forward_for_obstacles(root, steps = 5)
+			print obstacle
 			sys.stdout.flush()
-			self.window.pressKey(controls['right'])
-			if pit == obstacle == 0:
-				pass # self.window.pressKey(controls['right'])
-			else:
-				self.window.pressKey(controls['top'])
-				time.sleep(KEY_PRESS_SMALL * max(obstacle, pit))
-				self.window.releaseKey(controls['top'])
-				self.window.releaseKey(controls['right'])
+			if obstacle > 0:
+				self.window.pressKey(controls['jump'])
+				time.sleep(KEY_PRESS_SMALL * obstacle) if obstacle < 3 else time.sleep(KEY_PRESS * obstacle)
+				self.window.releaseKey(controls['jump'])
 		self.master.after(0, self.run)
 
 if __name__ == '__main__':
@@ -193,4 +173,4 @@ if __name__ == '__main__':
 		print 'Please enter window name.'
 	else:
 		name = sys.argv[1]
-		park = RunAI(name, 'mario')
+		park = RunAI(name, 'dino')
